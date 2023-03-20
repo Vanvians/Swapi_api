0148f1e3bed42a2c287d56eb83829a3e3604b323
jcezetah <jcezetah@gmail.com>
2023-03-20 17:32:46 +0100

    Minor adjustments

--- models/movie.go
+++ models/movie.go
@@ -4,10 +4,12 @@ import "time"
 
 type Movie struct{
 	ID string `json:"id"`
+	EpisodeID int `json:"episode_id"`
 	Title string `json:"title"`
 	Director string `json:"director"`
 	Producer string `json:"producer"`
 	ReleaseDate time.Time `json:"release_date"`
 	OpeningCrawl string `json:"opening_crawl"`
-	Characters string `json:"`
+	Characters []string `json:"characters"`
+	Comments []Comment `json:"comments"`
 }
